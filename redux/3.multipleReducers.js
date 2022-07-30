382f2cd64415b132d07ef7ac0ea23863a089095e
birendra20 <birendragupta20@gmail.com>
2022-07-30 19:00:37 +0530

    multiple reducers

--- redux/3.multipleReducers.js
+++ redux/3.multipleReducers.js
@@ -1,10 +1,11 @@
 const redux = require("redux");
-const createStore = redux.createStore;
 
-//1st define constant ,that indicate type of action
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
-//2nd we define our action and create action creater {function}
+
 function buyCake() {
   return {
     type: BUY_CAKE,
@@ -17,22 +18,29 @@ function buyIceCream() {
     type: BUY_ICECREAM,
   };
 }
-//3rd reducer function
 
-//initial state
-
-const initialState = {
+const initialCakeState = {
   numOfCakes: 10,
+};
+
+const initialIceCreamState = {
   numOfIceCreams: 20,
 };
 
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
-        ...state, //makes copies of the state
-        numOfCakes: state.numOfCakes - 1, //only update numOfCakes
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
       };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
     case BUY_ICECREAM:
       return {
         ...state,
@@ -42,10 +50,12 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
-
-//4th redux store(one store for entire application)
-//store accepts only one reducers
-const store = createStore(reducer);
+//combining multiple reducers as store accepts only one reducers as parameter
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+const store = createStore(rootReducer);
 console.log("Initial State ", store.getState());
 const unsubscribe = store.subscribe(() =>
   console.log("update store", store.getState())
