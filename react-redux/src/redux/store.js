65680177b9770b19253a70bb7561d74737e45c21
birendra20 <birendragupta20@gmail.com>
2022-07-31 18:56:48 +0530

    update

--- react-redux/src/redux/store.js
+++ react-redux/src/redux/store.js
@@ -1,7 +1,14 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+
+import logger from "redux-logger";
+
 import cakeReducer from "./cake/cakeReducer";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
